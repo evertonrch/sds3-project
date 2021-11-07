c6f61cd22d96246b396ae1b9d61c4bdadbda56b4
Everton Rocha <irocha10@hotmail.com>
2021-11-07 17:52:52 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -19,16 +19,9 @@ const DonutChart = () => {
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
     
-            setChartData({labels: myLabels, series: mySeries});
-        })
-    }, []);
-
-    
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+            setChartData({ labels: myLabels, series: mySeries});
+        });
+    }, []);    
     
     const options = {
         legend: {
